9625477919ad514ee0b50247ce11e60b30570bc4
jiacheng@home <jiacheng524@live.cn>
2024-04-27 21:45:55 +0800

    optimize import

--- spring/webflux/src/main/java/webflux/Application.java
+++ spring/webflux/src/main/java/webflux/Application.java
@@ -3,18 +3,9 @@ package webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Application {
