1a869bfc17a0ee2ef5f565baec7b97cb8d0836ad
Jordan_liu_enjoy_coding <jiacheng524@live.cn>
2025-06-05 11:35:39 +0800

    Update RouteConfig.java

--- spring/gateway/src/main/java/com/ljc/gw/RouteConfig.java
+++ spring/gateway/src/main/java/com/ljc/gw/RouteConfig.java
@@ -60,7 +60,7 @@ public class RouteConfig {
                         .path("/callapi/**")
                         .filters(f -> f.rewritePath("/callapi/(?<remaining>.*)", "/${remaining}")
                                 .filter(callApiFilter()))
-                        .uri("https://usertracking.chinawayltd.com/index.php?idSite=65&module=API&method=VisitsSummary.get&format=json&date=2023-05-07&period=day&token_auth=69deb5f74b1cbc0c5b0a7fc720e416d9&filter_limit=100"))
+                        .uri("https://test.com/index.php"))
                 .build();
     }
 
@@ -69,8 +69,8 @@ public class RouteConfig {
         return ((exchange, chain) -> {
             HttpHeaders headers = exchange.getRequest().getHeaders();
             if (headers.containsKey("call_api")) {
-                String apiUrl = "https://usertracking.huoyunren.com/piwik.php";
-                String requestBody = "{\"requests\":[\"?idsite=150070&send_image=0&res=1440x3200&ua=Dalvik%2F2.1.0%20%28Linux%3B%20U%3B%20Android%2013%3B%2022127RK46C%20Build%2FTKQ1.220905.001%29&url=https%3A%2F%2Fcom.gxt.ydt.driver%2Fcom.gxt.ydt.library.fragment.WaybillListFragment&rand=24603&rec=1&apiv=1&uid=579780346623561728&cdt=2023-09-26%2018%3A52%3A05%2B0800&action_name=WaybillListFragment&_id=2e68fbf757c54c5b&lang=zh\",\"?idsite=150070&send_image=0&res=1440x3200&ua=Dalvik%2F2.1.0%20%28Linux%3B%20U%3B%20Android%2013%3B%2022127RK46C%20Build%2FTKQ1.220905.001%29&url=https%3A%2F%2Fcom.gxt.ydt.driver%2Fcom.gxt.ydt.fragment.DriverWaybillTabFragment&rand=17126&rec=1&apiv=1&uid=579780346623561728&cdt=2023-09-26%2018%3A52%3A05%2B0800&action_name=%E8%AE%A2%E5%8D%95&_id=2e68fbf757c54c5b&lang=zh\",\"?_viewts=1695725214&idsite=150070&send_image=0&res=1440x3200&_idvc=14&ua=Dalvik%2F2.1.0%20%28Linux%3B%20U%3B%20Android%2013%3B%2022127RK46C%20Build%2FTKQ1.220905.001%29&url=https%3A%2F%2Fcom.gxt.ydt.driver%2Fcom.gxt.ydt.library.activity.WelcomeActivity&rand=20458&rec=1&apiv=1&_idts=1695722423&cdt=2023-09-26%2018%3A52%3A01%2B0800&action_name=56888%E4%B8%80%E7%82%B9%E9%80%9A%E5%8F%B8%E6%9C%BA&_id=2e68fbf757c54c5b&new_visit=1&lang=zh\",\"?idsite=150070&send_image=0&res=1440x3200&ua=Dalvik%2F2.1.0%20%28Linux%3B%20U%3B%20Android%2013%3B%2022127RK46C%20Build%2FTKQ1.220905.001%29&url=https%3A%2F%2Fcom.gxt.ydt.driver%2Fcom.gxt.ydt.activity.DriverActivity&rand=49313&rec=1&apiv=1&uid=579780346623561728&cdt=2023-09-26%2018%3A52%3A03%2B0800&action_name=56888%E4%B8%80%E7%82%B9%E9%80%9A%E5%8F%B8%E6%9C%BA&_id=2e68fbf757c54c5b&lang=zh\",\"?idsite=150070&send_image=0&res=1440x3200&ua=Dalvik%2F2.1.0%20%28Linux%3B%20U%3B%20Android%2013%3B%2022127RK46C%20Build%2FTKQ1.220905.001%29&url=https%3A%2F%2Fcom.gxt.ydt.driver%2Fcom.gxt.ydt.library.fragment.OrderListFragment&rand=16964&rec=1&apiv=1&uid=579780346623561728&cdt=2023-09-26%2018%3A52%3A03%2B0800&action_name=OrderListFragment&_id=2e68fbf757c54c5b&lang=zh\",\"?idsite=150070&send_image=0&res=1440x3200&ua=Dalvik%2F2.1.0%20%28Linux%3B%20U%3B%20Android%2013%3B%2022127RK46C%20Build%2FTKQ1.220905.001%29&url=https%3A%2F%2Fcom.gxt.ydt.driver%2Fcom.gxt.ydt.library.fragment.OrderFragment&rand=95751&rec=1&apiv=1&uid=579780346623561728&cdt=2023-09-26%2018%3A52%3A03%2B0800&action_name=%E8%B4%A7%E6%BA%90&_id=2e68fbf757c54c5b&lang=zh\"]}";
+                String apiUrl = "testUrl";
+                String requestBody = "test";
                 WebClient webClient = WebClient.create(apiUrl);
                 Object resp;
                 return webClient.post()
